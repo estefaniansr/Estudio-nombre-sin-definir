b6f72ca4f488dd8ffc190f8b3f322b5dc9a754d4
luciano <luciano.delapena@gmail.com>
2025-09-23 20:52:44 -0300

    oculte tabs en el login

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [
+    IonicModule, // Necesario para todos los componentes de Ionic
+    CommonModule  // Necesario para *ngIf y directivas de Angular
+  ]
 })
 export class TabsPage {
+  showTabBar = true;
 
-  constructor() {}
-
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Oculta el tab-bar si estás en tab1
+        this.showTabBar = !event.url.endsWith('/tab1');
+      }
+    });
+  }
 }
