1cbdd1dca048a34cb2f5fa90ee4226ef8f69b939
luciano <luciano.delapena@gmail.com>
2025-10-01 00:03:49 -0300

    cuando esta en el user aparecen los tabs y cuando esta en el login no

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -3,31 +3,40 @@ import { Router, NavigationEnd } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { Auth, onAuthStateChanged, User } from '@angular/fire/auth';
+import { auth } from '../../firebase-config'; // tu config de Firebase
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: true,
-  imports: [
-    IonicModule, // Necesario para todos los componentes de Ionic
-    CommonModule  // Necesario para *ngIf y directivas de Angular
-  ]
+  imports: [IonicModule, CommonModule]
 })
 export class TabsPage {
   showTabBar = false;
+  user: User | null = null;
 
   constructor(private router: Router) {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        // Oculta el tab-bar si estás en tab1
-        this.showTabBar = !event.url.endsWith('/tab1');
-          
-      }
-      
+    // Detecta usuario logueado
+    onAuthStateChanged(auth, (u) => {
+      this.user = u;
+      this.updateTabBar(this.router.url);
     });
-  }
-  
-  
-}
 
+    // Detecta cambios de ruta
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.updateTabBar(event.url);
+      });
+  }
+
+  private updateTabBar(url: string) {
+    // Si estamos en Tab1, solo mostrar barra si hay usuario
+    if (url.endsWith('/tab1')) {
+      this.showTabBar = !!this.user;
+    } else {
+      this.showTabBar = true; // otros tabs siempre muestran barra
+    }
+  }
+}
