81b774c8b52e18f0dbe62b48464083320d73e224
Leonel Godoy <leonelgodoy495@gmail.com>
2025-10-23 22:36:11 -0300

    auth.spec.ts agregado

--- src/app/services/auth.spec.ts
+++ src/app/services/auth.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service'; // 🔹 importar el nombre correcto
-//
 
 describe('AuthService', () => {
   let service: AuthService;
